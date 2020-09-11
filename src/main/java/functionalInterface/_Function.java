69f2e6ea6eef591315480d92be0cc4ec3108bf8f
mzma <mzma@gft.com>
2020-09-11 14:12:36 +0200

    Create interface Function, which put logic simple method incrementNumber()

---
+++ src/main/java/functionalInterface/_Function.java
@@ -0,0 +1,21 @@
+package functionalInterface;
+
+import java.util.function.Function;
+
+public class _Function {
+
+    public static void main(String[] args) {
+        int increment = incrementByOne(0);
+        System.out.println(increment);
+
+        Integer increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number + 1;
+
+    static int incrementByOne(int number) {
+        return number + 1;
+    }
+}
